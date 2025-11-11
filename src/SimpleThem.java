4d2ce2d742abe27791127561fec2e5aca11e61aa
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-11 23:38:24 +0530

    SimpleThem code is ready

---
+++ src/SimpleThem.java
@@ -0,0 +1,11 @@
+public class SimpleThem {
+    String name;
+    String[] commonAttacks;
+
+    SimpleThem(String name, String[] commonAttacks) {
+        this.name = name;
+        this.commonAttacks = commonAttacks;
+
+    }
+
+}
